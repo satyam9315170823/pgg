c210d1bab57f9074a48dca93f7cbd9f9e96964a0
igortasic123 <63684263+igortasic123@users.noreply.github.com>
2025-02-23 15:06:15 -0500

    youtube-code-commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from 'next/font/google'
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/next';
+
 
 const font = Plus_Jakarta_Sans({
   weight : '400',
@@ -22,7 +24,9 @@ export default function RootLayout({
       <body
         className={font.className}
       >
+
         {children}
+        <Analytics />
       </body>
     </html>
   );
