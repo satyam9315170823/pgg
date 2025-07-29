25236d3a6f1bf8548dec0f2d180c850feef871f9
satyam kumar <satyam.kumar@acem.edu.in>
2025-07-29 08:31:49 +0530

    hf

--- app/expert/page.tsx
+++ app/expert/page.tsx
@@ -25,7 +25,7 @@ const WebsiteDesign = () => {
         </p>
       </div>
 
-      <BentoGrid className="max-w-4xl mx-2">
+      <BentoGrid className="max-w-4xl mx-auto">
         {items.map((item, i) => (
           <BentoGridItem
             key={i}
