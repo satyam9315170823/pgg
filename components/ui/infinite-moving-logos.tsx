118711657d4d2ff324bd0e06f8e855b43a3dc726
satyam kumar <satyam.kumar@acem.edu.in>
2025-08-05 17:16:05 +0530

    gh

--- components/ui/infinite-moving-logos.tsx
+++ components/ui/infinite-moving-logos.tsx
@@ -61,7 +61,7 @@ export const InfiniteMovingLogos = ({
   const getSpeed = () => {
     if (containerRef.current) {
       if (speed === "fast") {
-        containerRef.current.style.setProperty("--animation-duration", "20s");
+        containerRef.current.style.setProperty("--animation-duration", "15s");
       } else if (speed === "normal") {
         containerRef.current.style.setProperty("--animation-duration", "40s");
       } else {
